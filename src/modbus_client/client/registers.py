b6d18c41f9f09c957670c5ccb322b94301d20b83
Krystian Dużyński <krystian.duzynski@gmail.com>
2024-01-15 21:44:17 +0100

    client: refactored type converters

--- src/modbus_client/client/registers.py
+++ src/modbus_client/client/registers.py
@@ -1,6 +1,7 @@
 import struct
 from abc import abstractmethod
-from typing import Union, List, Optional, Tuple, cast, Callable, Any
+from dataclasses import dataclass
+from typing import Union, List, Optional, cast, Callable, Any, Dict
 
 from modbus_client.client.address_range import AddressRange
 from modbus_client.client.types import ModbusReadSession, RegisterType, RegisterValueType
@@ -8,25 +9,30 @@ from modbus_client.client.types import ModbusReadSession, RegisterType, Register
 BitsArray = List[int]
 
 
-def get_type_format(reg_type: RegisterValueType) -> Tuple[str, bool, Callable[[Any], Union[int, float]]]:
-    if reg_type == RegisterValueType.S16:
-        return "h", False, round
-    elif reg_type == RegisterValueType.U16:
-        return "H", False, round
-    elif reg_type == RegisterValueType.S32BE:
-        return "i", True, round
-    elif reg_type == RegisterValueType.U32BE:
-        return "I", True, round
-    elif reg_type == RegisterValueType.S32LE:
-        return "i", False, round
-    elif reg_type == RegisterValueType.U32LE:
-        return "I", False, round
-    elif reg_type == RegisterValueType.F32BE:
-        return "f", True, float
-    elif reg_type == RegisterValueType.F32LE:
-        return "f", False, float
-    else:
-        raise Exception("invalid type")
+@dataclass
+class RegisterTypeConverter:
+    format_str: str
+    reverse_bytes: bool
+    converter_func: Callable[[Any], Union[int, float]]
+
+
+register_type_converters: Dict[RegisterValueType, RegisterTypeConverter] = {
+    RegisterValueType.S16: RegisterTypeConverter("h", False, round),
+    RegisterValueType.U16: RegisterTypeConverter("H", False, round),
+    RegisterValueType.S32BE: RegisterTypeConverter("i", True, round),
+    RegisterValueType.U32BE: RegisterTypeConverter("I", True, round),
+    RegisterValueType.S32LE: RegisterTypeConverter("i", False, round),
+    RegisterValueType.U32LE: RegisterTypeConverter("I", False, round),
+    RegisterValueType.F32BE: RegisterTypeConverter("f", True, float),
+    RegisterValueType.F32LE: RegisterTypeConverter("f", False, float),
+}
+
+
+def get_type_format(reg_type: RegisterValueType) -> RegisterTypeConverter:
+    reg_type_converter = register_type_converters.get(reg_type)
+    if reg_type_converter is None:
+        raise ValueError(f"Unknown register type {reg_type}")
+    return reg_type_converter
 
 
 def get_bits(value: int, bits: BitsArray) -> int:
@@ -39,7 +45,7 @@ def get_bits(value: int, bits: BitsArray) -> int:
 class IRegister(AddressRange):
     def __init__(self, name: str, reg_type: RegisterType, address: int,
                  value_type: RegisterValueType, bits: Optional[BitsArray]) -> None:
-        super().__init__(address, struct.calcsize(get_type_format(value_type)[0]) // 2)
+        super().__init__(address, struct.calcsize(get_type_format(value_type).format_str) // 2)
         self.name = name
         self.reg_type = reg_type
         self.value_type = value_type
@@ -50,13 +56,14 @@ class IRegister(AddressRange):
         pass
 
     def get_raw_from_read_session(self, read_session: ModbusReadSession) -> int:
-        type_format, reverse, _ = get_type_format(self.value_type)
-        count = struct.calcsize(type_format) // 2
+        reg_type_converter = get_type_format(self.value_type)
+        count = struct.calcsize(reg_type_converter.format_str) // 2
 
         registers_unordered = [read_session.registers_dict[(self.reg_type, self.address + i)] for i in range(count)]
-        registers_ordered = registers_unordered if not reverse else reversed(registers_unordered)
+        registers_ordered = registers_unordered if not reg_type_converter.reverse_bytes else reversed(
+            registers_unordered)
         value_bytes = struct.pack("<" + "H" * count, *registers_ordered)
-        val = cast(int, struct.unpack("<" + type_format, value_bytes)[0])
+        val = cast(int, struct.unpack("<" + reg_type_converter.format_str, value_bytes)[0])
 
         if self.bits is None:
             return val
@@ -64,13 +71,14 @@ class IRegister(AddressRange):
             return get_bits(val, self.bits)
 
     def value_to_modbus_registers(self, value: Union[int, float]) -> List[int]:
-        type_format, reverse, value_type = get_type_format(self.value_type)
-        count = struct.calcsize(type_format) // 2
+        reg_type_converter = get_type_format(self.value_type)
+        count = struct.calcsize(reg_type_converter.format_str) // 2
 
-        raw_value = value_type(value)
-        value_bytes = struct.pack("<" + type_format, raw_value)
+        raw_value = reg_type_converter.converter_func(value)
+        value_bytes = struct.pack("<" + reg_type_converter.format_str, raw_value)
         registers_unordered = list(struct.unpack("<" + "H" * count, value_bytes))
-        registers_ordered = registers_unordered if not reverse else list(reversed(registers_unordered))
+        registers_ordered = registers_unordered if not reg_type_converter.reverse_bytes else list(
+            reversed(registers_unordered))
 
         return registers_ordered
 
